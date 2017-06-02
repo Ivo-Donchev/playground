a085dc760d29aab849cfaf1061bc31fbb73e4383
Ivo-Donchev <ivodonchev267@gmail.com>
2017-06-03 00:03:04 +0300

    Add algorithms folder with sorting algos in cpp

---
+++ algorithms/sorting/main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+void swap(int& a, int& b)
+{
+  int c = a;
+  a = b;
+  b = c;
+}
+
+
+void printArr(int *arr, int size)
+{
+  for(int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+
+  cout << endl;
+}
+
+
+void insertionSort(int* arr, int size)
+{
+  for(int i = 0; i < size - 1; i++)
+  {
+    for(int j = i + 1; j < size; j++)
+    {
+      if(arr[i] > arr[j])
+      {
+        int k = j;
+
+        while(k)
+        {
+          swap(arr[k], arr[k-1]);
+          k--;
+        }
+      }
+    }
+  }
+}
+
+
+void bubbleSort(int* arr, int size)
+{
+
+  for(int i = 0; i < size - 1; i++)
+  {
+    int min = i;
+
+    /* Find the index if min element */
+    for(int j = i + 1; j < size; j++)
+    {
+      if(arr[j] < arr[min])
+      {
+        min = j;
+      }
+    }
+
+    while(min - i)
+    {
+      swap(arr[min], arr[min-1]);
+      min--;
+    }
+  }
+}
+
+int selectionSort(int* arr, int size)
+{
+  for(int i = 0; i < size - 1; i++)
+  {
+    int min = i;
+
+    /* Find the index if min element */
+    for(int j = i + 1; j < size; j++)
+    {
+      if(arr[j] < arr[min])
+      {
+        min = j;
+      }
+    }
+    swap(arr[i], arr[min]);
+  }
+}
+
+
+int main()
+{
+  const int size = 30;
+  int* arr = new int [size];
+  for(int i = 0; i < size; i++)
+  {
+    arr[i] = size - i;
+  }
+
+  printArr(arr, (int)size);
+  selectionSort(arr, (int)size);
+
+
+  return 0;
+}
