5a092e73831eb9078e2ec14ccdc6c50cd84fd1e0
Ivo-Donchev <ivodonchev267@gmail.com>
2017-08-05 18:17:34 +0300

    Define the interface of CommandParser class

---
+++ ruby/fmi_course/exercises/3/cli_micro_library.rb
@@ -0,0 +1,21 @@
+class CommandParser
+	def initialize(command_name)
+
+	end
+
+  def argument()
+
+  end
+
+  def option()
+
+  end
+
+  def option_with_parameter()
+
+  end
+
+  def parse(command_runner, argv)
+
+  end
+end
