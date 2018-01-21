708ea36fdd3554b62343e120c4d7c87f2280d065
Ivo-Donchev <ivo_9500@abv.bg>
2018-01-21 17:15:35 +0200

    Implement bash script for collecting all songs from youtube playlist

---
+++ bash/playlist-collector.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DIRECTORY="songs"
+PLAYLIST_LINK=""
+
+for i in "$@"
+do
+case $i in
+    -d=*|--directory=*)
+    DIRECTORY="${i#*=}"
+    shift # past argument=value
+    ;;
+    -p=*|--playlist=*)
+    PLAYLIST_LINK="${i#*=}"
+    shift # past argument=value
+    ;;
+  *)
+    ;;
+esac
+done
+
+
+# Create destination directory if not exists
+mkdir -p ${DIRECTORY}
+
+
+SONGS_LINKS=$(curl ${PLAYLIST_LINK} | grep -Po '(?<=href=")[^"]*' | grep index)
+
+i=0
+
+for song in ${SONGS_LINKS}
+do
+  link="https://www.youtube.com${song}"
+  DESTINATION="${DIRECTORY}/song${i}.mp3"
+
+  echo DOWNLOADING ${link}\n
+  echo ${DESTINATION}
+
+  youtube-dl --extract-audio --audio-format mp3 "$link" --output ${DESTINATION}
+  i="$[$i+1]"
+done
